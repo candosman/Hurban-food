141d319a2c7d2d97a208a78608c6fefb6c103966
David Fonseca <davidfonseca042007@gmail.com>
2024-05-21 15:17:53 +0400

    save

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create!(name: "KFC", address: "Moka", phone: "0602650362")
+Restaurant.create!(name: "Buger King", address: "Port Louis", phone: "0583756082")
+Restaurant.create!(name: "subway", address: "Vacoas Phoenix", phone: "0184729685")

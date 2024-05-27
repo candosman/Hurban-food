6fd726681031b9bfc58e8e5b5f757b85e9aa934d
siddhish Jogiah <sidjogiah@gmail.com>
2024-05-27 11:31:54 +0400

    order numbers in order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_062727) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_070926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_062727) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_number"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 

3de178586bdb93369a0994f33a60aba477c2416b
Bryan Chuttoo <chuttoobryan04@gmail.com>
2024-05-27 16:12:12 +0400

    front

--- app/controllers/restaurants_reviews_controller.rb
+++ app/controllers/restaurants_reviews_controller.rb
@@ -15,32 +15,3 @@ class RestaurantsReviewsController < ApplicationController
 
   end
 end
-def create
-  @restaurant = Restaurant.find(params[:restaurant_id])
-  @review = RestaurantReview.new(review_params)
-  @review.user = current_user
-  @review.restaurant = @restaurant
-  if @review.save
-    redirect_to restaurant_path(@restaurant)
-  else
-    render restaurant_path(@restaurant), status: :unprocessable_entity
-  end
-end
-def review_params
-  params.require(:review).permit(:content, :rating)
-
-end  def create
-@restaurant = Restaurant.find(params[:restaurant_id])
-@review = RestaurantReview.new(review_params)
-@review.user = current_user
-@review.restaurant = @restaurant
-if @review.save
-  redirect_to restaurant_path(@restaurant)
-else
-  render restaurant_path(@restaurant), status: :unprocessable_entity
-end
-end
-def review_params
-params.require(:review).permit(:content, :rating)
-
-end
